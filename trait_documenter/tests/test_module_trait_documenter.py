7affa99609012ad0d725225b26c93a64f100c5fc
Ioannis Tziakos <ioannist@enthought.com>
2015-06-25 01:31:22 +0100

    fix tests

--- trait_documenter/tests/test_module_trait_documenter.py
+++ trait_documenter/tests/test_module_trait_documenter.py
@@ -13,31 +13,26 @@ class TestModuleTraitDocumenter(unittest.TestCase):
 
     def test_can_document_member(self):
         can_document_member = ModuleTraitDocumenter.can_document_member
-        parent = ModuleDocumenter(mock.Mock(), 'test')
+        parent = ModuleDocumenter(mock.Mock(), u'test')
 
         # modules
         parent.object = test_file
-        self.assertFalse(can_document_member(Dummy, 'Dummy', True, parent))
-        self.assertFalse(can_document_member(Dummy, 'Dummy', False, parent))
+        self.assertFalse(can_document_member(Dummy, u'Dummy', True, parent))
+        self.assertFalse(can_document_member(Dummy, u'Dummy', False, parent))
         self.assertFalse(can_document_member(
-            test_file.module_trait, 'module_trait', False, parent))
+            test_file.module_trait, u'module_trait', False, parent))
         self.assertTrue(can_document_member(
-            test_file.module_trait, 'module_trait', True, parent))
+            test_file.module_trait, u'module_trait', True, parent))
 
         # class
         parent.object = Dummy
         self.assertFalse(
             can_document_member(
-                Dummy.class_traits()['trait_1'], 'trait_1', True, parent))
-        self.assertFalse(
-            can_document_member(
-                Dummy.class_traits()['trait_1'], 'trait_1', False, parent))
+                Dummy.class_traits()[u'trait_1'], u'trait_1', False, parent))
 
     def test_import_object(self):
         # given
-        documenter = ModuleTraitDocumenter(mock.Mock(), 'test')
-        value = mock.Mock(return_value=[])
-        documenter.env.config.autodoc_mock_imports = value
+        documenter = ModuleTraitDocumenter(mock.Mock(), u'test')
         documenter.modname = u'trait_documenter.tests.test_file'
         documenter.objpath = [u'long_module_trait']
 
@@ -51,11 +46,12 @@ class TestModuleTraitDocumenter(unittest.TestCase):
 
     def test_add_directive_header(self):
         # given
-        documenter = ModuleTraitDocumenter(mock.Mock(), '_test')
-        documenter.parent = Dummy
-        documenter.object_name = 'trait_2'
-        documenter.modname = u'trait_documenter._tests._test_file'
-        documenter.objpath = [u'Dummy', u'trait_2']
+        documenter = ModuleTraitDocumenter(mock.Mock(), u'test')
+        documenter.parent = test_file
+        documenter.options = mock.Mock(annotation=False)
+        documenter.modname = u'trait_documenter.tests.test_file'
+        documenter.object_name = u'long_module_trait'
+        documenter.objpath = [u'long_module_trait']
         documenter.add_line = mock.Mock()
 
         # when
@@ -63,10 +59,10 @@ class TestModuleTraitDocumenter(unittest.TestCase):
 
         # then
         expected = [
-            (u'.. py:attribute:: Dummy.trait_2', u'<autodoc>'),
+            (u'.. py:data:: long_module_trait', u'<autodoc>'),
             (u'   :noindex:', u'<autodoc>'),
-            (u'   :module: trait_documenter._tests._test_file', u'<autodoc>'),
-            (u"   :annotation: = Property(Float,depends_on='trait_1')", u'<autodoc>')]  # noqa
+            (u'   :module: trait_documenter.tests.test_file', u'<autodoc>'),
+            (u'   :annotation: = Range(low=0.2,high=34)', u'<autodoc>')]  # noqa
         calls = documenter.add_line.call_args_list
         for index, call in enumerate(calls):
             self.assertEqual(tuple(call)[0], expected[index])
