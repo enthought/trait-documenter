3d3e6c46b86634f8537a3c8bffa8f7d24dafb6a2
Ioannis Tziakos <mail@itziakos.gr>
2018-09-22 14:28:34 +0100

    Do not try to use alias

---
+++ trait_documenter/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM TRAIT-DOCUMENTER SETUP.PY
+version = '1.1.1'
+full_version = '1.1.1.dev14'
+git_revision = '83c3d71'
+is_released = False
+
+if not is_released:
+    version = full_version
